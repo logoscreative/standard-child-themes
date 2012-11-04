e7c628cebf7dab6b9ce78c468a61800332f38774
Cliff Seal <cliff@logos-creative.com>
2012-11-04 09:40:19 -0500

    Update to Bootstrap 2.2.1

--- bootstrap-two-two/functions.php
+++ bootstrap-two-two/functions.php
@@ -1,18 +1,16 @@
 <?php
 
-/* Update to Bootstrap 2.1 */
+/* Update to Bootstrap 2.2.1 */
 
 function update_bootstrap() {
-	
+
 	wp_dequeue_style( 'standard' );
-	
+
 	wp_dequeue_style( 'bootstrap' ); 
 	wp_register_style( 'bootstrap-two', get_stylesheet_directory_uri() . '/css/lib/bootstrap.min.css' );
 	wp_enqueue_style( 'bootstrap-two' );
 	
 	wp_dequeue_style( 'bootstrap-responsive' );
-	wp_register_style( 'bootstrap-responsive-two', get_stylesheet_directory_uri() . '/css/lib/bootstrap-responsive.min.css' );
-	wp_enqueue_style( 'bootstrap-responsive-two' );
 	
 	wp_dequeue_script( 'bootstrap' );
 	wp_register_script( 'bootstrap-two', get_stylesheet_directory_uri() . '/js/lib/bootstrap.min.js', array( 'jquery' ) );
